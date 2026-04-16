30bc6e73fba66de236e2428a174ce159184b7198
jrippeon <jrippeon@terpmail.umd.edu>
2026-04-15 21:47:51 -0400

    more stuff

--- hw7/hw7q1.py
+++ hw7/hw7q1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #%% Create our domain.
 
-h= 0.1
+h= 0.04
 
 def fh(p):
     p= np.array(p)
@@ -32,8 +32,14 @@ pts, tri= dm.distmesh2D(
     bbox=(-1,1,-1,1)
 )
 
-# find the boundary points
 
+#%% save / load the mesh
+# np.savetxt('pts.csv', pts)
+# np.savetxt('tri.csv', tri, fmt='%d')
+pts= np.loadtxt('pts.csv')
+tri= np.loadtxt('tri.csv', dtype=int)
+
+#%% find the boundary points
 bdy_tol= 1e-6
 N_idx= np.nonzero(pts[:,0] > 1 - bdy_tol)
 S_idx= np.nonzero(pts[:,0] < -1 + bdy_tol)
@@ -41,18 +47,32 @@ E_idx= np.nonzero(pts[:,1] > 1 - bdy_tol)
 W_idx= np.nonzero(pts[:,1] < -1 + bdy_tol)
 boundary= np.union1d(N_idx,np.union1d(S_idx, np.union1d(E_idx, W_idx)))
 
-# %% 
-plt.scatter(pts[:,0], pts[:,1])
+# %% plot mesh
+# plt.scatter(pts[:,0], pts[:,1], s=1)
 plt.triplot(pts[:,0], pts[:,1], tri)
-plt.scatter(pts[boundary,0], pts[boundary,1], c='r')
+plt.scatter(pts[boundary,0], pts[boundary,1], c='r', s=5)
+plt.gca().set_aspect('equal')
 
-# %%
+# %% computing solution with homogeneous dirichlet conditions
+Npts= pts.shape[0]
 
-u= fem.ginzburg_landau(pts, tri, boundary)
+u_init= -np.ones(Npts)
+u_init[boundary]= 0
+u= fem.ginzburg_landau(pts, tri, boundary, u_init=u_init)
 
+ax= plt.figure().add_subplot(projection='3d')
+ax.plot_trisurf(pts[:,0], pts[:,1], tri, u)
 
-print(u)
-# %%
-plt.tricontourf(pts[:,0], pts[:,1], tri, u)
+# %% computing solution with frustrated dirichlet conditions
+Npts= pts.shape[0]
+u_init= -np.ones(Npts)
+u_init[N_idx]= -1
+u_init[S_idx]= -1
+u_init[E_idx]= 1
+u_init[W_idx]= 1
+uD= u_init[boundary]
+u_frustrated= fem.ginzburg_landau(pts, tri, boundary, uD=uD)
 
+ax= plt.figure().add_subplot(projection='3d')
+ax.plot_trisurf(pts[:,0], pts[:,1], tri, u_frustrated)
 # %%
