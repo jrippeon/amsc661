ce9fcc66ffcf0a3004f8bc1e2d9a00e33b1a89d4
jrippeon <jrippeon@terpmail.umd.edu>
2026-04-22 20:32:16 -0400

    add plots

--- hw8/hw8q2.py
+++ hw8/hw8q2.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-# %% Set up the problem
+#%%% Set up the problem %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 n= 100
 h=  2 / (n+1)
 ### IMPORTANT: I'm going to have xi of length n+2 so the indices line up
@@ -54,42 +54,107 @@ def F(t, w):
     return result
 
 
-# %% obtain solutions
+#%%% obtain solutions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-u0_exact= 1 - xi**2
-w0_exact= np.zeros(n+4)
-w0_exact[:n+2]= u0_exact
-w0_exact[-2]= -1
-w0_exact[-1]= 1
+t_eval= np.arange(0, 1.2, 0.1)
 
-u0_other= 1 - 0.99 * np.cos(2 * np.pi * xi)
-w0_other= np.zeros(n+4)
-w0_other[:n+2]= u0_other
-w0_other[-2]= -1
-w0_other[-1]= 1
+def w0(f):
+    u0= f(xi)
+    w0= np.zeros(n+4)
+    w0[:n+2]= u0
+    w0[-2]= -1
+    w0[-1]= 1
+
+    return w0
+
+w0_exact= w0(lambda x: 1 - x**2)
+w0_other= w0(lambda x: 1 - 0.99 * np.cos(2 * np.pi * x))
 
 
-sol= solve_ivp(F, [0, 1.2], w0_other, method='BDF')
+sol_exact= solve_ivp(F, [0, 1.2], w0_exact, method='BDF', t_eval=t_eval)
+sol_other= solve_ivp(F, [0, 1.2], w0_other, method='BDF', t_eval=t_eval)
 
-# %% post-processing
+#%%% plotting functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t= sol.t
-y= sol.y
-# first coord is time, second is space
-y= y.T
-xR= y[:, -2]
-xL= y[:, -1]
-y= y[:, :-2]
+def post_process(sol):
+    t= sol.t
+    y= sol.y
+    # first coord is time, second is space
+    y= y.T
+    xR= y[:, -2]
+    xL= y[:, -1]
+    u= y[:, :-2]
 
-# undo the change of variables to get x(t) from ξ
-xo= 1/2 * (xL + xR)
-xf= 1/2 * (xL - xR)
-x= xi[None, :] * xf[:, None] + xo[:, None]
-print(f'{x.shape=}')
-print(f'{t.shape=}')
-print(f'{y.shape=}')
+    # undo the change of variables to get x(t) from ξ(t)
+    xo= 1/2 * (xL + xR)
+    xf= 1/2 * (xL - xR)
+    x= xi[None, :] * xf[:, None] + xo[:, None]
 
-ax= plt.figure().add_subplot(projection='3d')
-ax.plot_surface(x, t[:, None], y)
+    return t, x, u
+
+def plot_figures_normalized(sol, xi):
+    n= len(xi) - 2
+    t, x, u= post_process(sol)
+
+    # get u(ξ,t) / u_max (t)
+    u_max= np.max(u, axis=-1)
+    u_normalized= u / u_max[:, None]
+
+    # pretty colors
+    n= len(t)
+    colors= plt.cm.turbo(np.linspace(0.2, 0.8, n))
+
+    fig, ax= plt.subplots()
+
+    for i in range(len(t)):
+        ax.plot(xi,u_normalized[i], color=colors[i])
+
+    ax.plot(xi, 1 - xi**2, color='k', linestyle=':')    
+    legend_labels= [f't={ti:.1f}' for ti in t] + [r'$1 - \xi^2$']
+    ax.legend(legend_labels, bbox_to_anchor=(1, 1))
+    ax.set_xlabel(r'$\xi$')
+    ax.set_ylabel(r'$\frac{u(\xi, t)}{u_\text{max}(t)}$')
+    return fig, ax
+
+def plot_figures(sol, xi):
+    n= len(xi) - 2
+    t, x, u= post_process(sol)
+
+    # pretty colors
+    n= len(t)
+    colors= plt.cm.turbo(np.linspace(0.2, 0.8, n))
+
+    fig, ax= plt.subplots()
+
+    for i in range(len(t)):
+        ax.plot(x[i], u[i], color=colors[i])
+    legend_labels= [f't={ti:.1f}' for ti in t]
+    plt.legend(legend_labels, bbox_to_anchor=(1,1))
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$u(x, t)$')
+    return fig, ax
+
+
+
+#%%% Create Plots %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+_, ax= plot_figures(sol_exact, xi)
+ax.set_title(f'Solution with parabolic IC')
+plt.savefig("../../HW8/exact.eps", bbox_inches="tight")
 plt.show()
 
+_, ax= plot_figures_normalized(sol_exact, xi)
+ax.set_title(f'Normalized solution with parabolic IC')
+plt.savefig("../../HW8/exact_normalized.eps", bbox_inches="tight")
+plt.show()
+
+_, ax= plot_figures(sol_other, xi)
+ax.set_title(f'Solution with sinusoidal IC')
+plt.savefig("../../HW8/other.eps", bbox_inches="tight")
+plt.show()
+
+_, ax= plot_figures_normalized(sol_other, xi)
+ax.set_title(f'Normalized solution with sinusoidal IC')
+plt.savefig("../../HW8/other_normalized.eps", bbox_inches="tight")
+plt.show()
+# %%
--- hw8/hw8q3.py
+++ hw8/hw8q3.py
@@ -5,56 +5,78 @@ import matplotlib.pyplot as plt
 import distmesh as dm
 from importlib import reload
 from matplotlib.animation import FuncAnimation
+from time import time
 
-# %% create the mesh
+#%%% create the mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-h= 0.1
-
-sdf= dm.ddiff(
-    dm.dcircle(0, 0, 2),
-    dm.dcircle(0, 0, 1)
-)
-
-def f(p):
+def f_unif_ones(p):
     p= np.array(p)
     n, _= p.shape
     return np.ones(n)
 
-theta= np.linspace(0, 2 * np.pi, 10)
-pfix= np.array([np.sin(theta), np.cos(theta)]).T
+def f_unif_zeros(p):
+    p= np.array(p)
+    n, _= p.shape
+    return np.zeros(n)
 
-pts, tri= dm.distmesh2D(
-    fd=sdf,
-    fh=f,
-    h0=h,
-    pfix=pfix,
-    bbox=(-2,2,-2,2)
-)
+# h= 0.05
 
-# %% locate boundary nodes
+# sdf= dm.ddiff(
+#     dm.dcircle(0, 0, 2),
+#     dm.dcircle(0, 0, 1)
+# )
+
+
+# theta= np.linspace(0, 2 * np.pi, 10)
+# pfix= np.array([np.sin(theta), np.cos(theta)]).T
+
+# pts, tri= dm.distmesh2D(
+#     fd=sdf,
+#     fh=f_unif_ones,
+#     h0=h,
+#     pfix=pfix,
+#     bbox=(-2,2,-2,2)
+# )
+#%%% Save / Load mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# np.savetxt('pts.csv', pts)
+# np.savetxt('tri.csv', tri, fmt='%d')
+pts= np.loadtxt('pts.csv')
+tri= np.loadtxt('tri.csv', dtype=int)
+
+#%%% locate boundary nodes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 boundary_tol= 1e-6
 r= np.linalg.norm(pts, axis=-1)
 inner_boundary= np.nonzero(r < 1 + boundary_tol)
 outer_boundary= np.nonzero(r > 2 - boundary_tol)
 boundary= np.union1d(inner_boundary, outer_boundary)
 
-# %% visualize mesh
+#%%% visualize mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # plt.triplot(pts[:,0], pts[:,1], tri)
 # plt.scatter(pts[boundary, 0], pts[boundary, 1], c='r')
 # plt.show()
 
-
-# %% initial condition
+#%%% initial condition %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+r= np.linalg.norm(pts, axis=-1)
 phi= np.atan2(pts[:,1], pts[:,0])
 
 u0= r + np.cos(phi)
 
-# %% run the code
+# #%%% Wacky BC (just for fun) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# u0[inner_boundary]= 10 * np.cos(phi[inner_boundary])
+# u0[outer_boundary]= 10 * np.sin(phi[outer_boundary])
+# uD= u0[boundary]
+
+#%%% Normal BC %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+u0[boundary]= 0
+uD= u0[boundary]
+
+#%%% run the code %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 reload(fem)
 dt= 0.01
 t_max= 1
 
 # we can reuse the f from above here, just needs to return 1 lol
+f= f_unif_ones
 
 t, u= fem.heat(
     pts=pts,
@@ -63,33 +85,77 @@ t, u= fem.heat(
     f=f,
     u0=u0,
     dt=dt,
-    t_max=t_max
+    t_max=t_max, 
+    uD=uD
 )
-# %%
-fig, ax= plt.subplots()
-ax.set_aspect('equal')
-mesh= ax.tripcolor(pts[:,0], pts[:,1], tri, u[50], shading='gouraud')
-mesh.set_array(u[50])
-fig.colorbar(mesh, ax=ax)
-# %% cute animation 
-#(AI CODE WARNING, TOO LAZY TO FIGURE THIS OUT ON MY OWN FOR A RANDOM VISUALIZATION)
-fig, ax= plt.subplots()
-ax.set_aspect('equal')
-print(f'{u.shape=}')
+#%%% For plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def plot_surface(pts, tri, t, u, t_plot):
+    '''
+    Plot a surface of u at time t_plot
+    '''
 
-mesh= ax.tripcolor(pts[:,0], pts[:,1], tri, u[0], shading='gouraud')
-fig.colorbar(mesh, ax=ax)
-mesh.set_clim(vmin=u.min(), vmax=u.max())
+    # find the index of the nearest time
+    i_plot= np.abs(t - t_plot).argmin()
 
-def update(frame):
-    mesh.set_array(u[frame])
-    ax.set_title(f't = {t[frame]:.2f}')
-    return mesh, # this comma is apparently crucial, we need this to return an iterable (tuple)
+    fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+    ax.plot_trisurf(pts[:, 0], pts[:, 1], u[i_plot], cmap='magma')
+    ax.set_title(f'$u$ vs $(x,y)$ at $t={t_plot}$')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('u')
+    return fig, ax
+def plot_radial(pts, tri, t, u, t_plot):
+    # find the index of the nearest time
+    i_plot= np.abs(t - t_plot).argmin()
 
-ani= FuncAnimation(fig, update, frames=u.shape[0], interval=50)
+    r= np.linalg.norm(pts, axis=-1)
+    r_sorted_indices= np.argsort(r)
+    fig, ax= plt.subplots()
+    ax.scatter(r[r_sorted_indices], u[i_plot, r_sorted_indices], s=3, c=u[i_plot, r_sorted_indices], cmap='magma')
+    ax.set_title(f'$u$ vs $r$ at $t={t_plot}$')
+    ax.set_xlabel('r')
+    ax.set_ylabel('u')
+    return fig, ax
+#%%% Generate plots %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fig, ax= plot_surface(pts, tri, t, u, 0.1)
+plt.show()
+fig, ax= plot_surface(pts, tri, t, u, 1)
 plt.show()
 
+# radial graph
+fig, ax= plot_radial(pts, tri, t, u, 1.1)
+# plot exact solution
+r= np.linspace(1, 2, 100)
+u_exact= (1 - r**2)/4 + 3 * np.log(r) / (4 * np.log(2))
+ax.plot(r, u_exact, color='k', linestyle=':')
+plt.show()
 # %%
-ani.save('sol.mp4', fps=30)
 
-# %%
+
+# Nonsense for testing
+# # %%
+# fig, ax= plt.subplots()
+# ax.set_aspect('equal')
+# mesh= ax.tripcolor(pts[:,0], pts[:,1], tri, u[50], shading='gouraud', cmap='magma')
+# mesh.set_array(u[50])
+# fig.colorbar(mesh, ax=ax)
+
+# # %% cute animation 
+# #(AI CODE WARNING, TOO LAZY TO FIGURE THIS OUT ON MY OWN FOR A RANDOM VISUALIZATION)
+# fig, ax= plt.subplots()
+# ax.set_aspect('equal')
+
+# mesh= ax.tripcolor(pts[:,0], pts[:,1], tri, u[0], shading='gouraud', cmap='magma')
+# fig.colorbar(mesh, ax=ax)
+# mesh.set_clim(vmin=u.min(), vmax=u.max())
+
+# def update(frame):
+#     mesh.set_array(u[frame])
+#     ax.set_title(f't = {t[frame]:.2f}')
+#     return mesh, # this comma is apparently crucial, we need this to return an iterable (tuple)
+
+# ani= FuncAnimation(fig, update, frames=u.shape[0], interval=50)
+# plt.show()
+
+# # %%
+# ani.save('sol.mp4', fps=30)
